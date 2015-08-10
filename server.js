7a91b7f27a36b81e004c2142bbf934dcb54c0c8b
CodeMaster7 <italianstalian_2@yahoo.com>
2015-08-10 15:16:28 -0600

    2nd commit

---
+++ server.js
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////////
+/////////////       Dependencies
+////////////////////////////////////////////////////////
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
+var twilio = require('twilio');
+var firebase = require('firebase');
+
+var app = express();
+var port =3000;
+
+// Twilio Credentials 
+var accountSid = ''; 
+var authToken = ''; 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+
+/////////////////////////////////////////////////////////
+/////////////       Middleware
+////////////////////////////////////////////////////////
+
+app.use(express.static('public'));
+///// app.use(bodyparser());      ------normal
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+var message = {
+    "message": "Hello world"
+}
+
+/////////////////////////////////////////////////////////
+/////////////       Endpoints
+////////////////////////////////////////////////////////
+app.get('/api/message', function (req, res) {
+    return res.send({"message": "hello user!!!!"});
+});
+app.post('/api/receive_message', function (req, res) {
+    console.log(req.body.message);
+    res.send();
+});
+
+app.post('/api/send_text_message', function (req, res) {
+    console.log(req.body.message);
+//    request.post('https://' + accountSid + ':' + authToken + '@api.twilio.com/2010-04-01/Accounts/' + accountSid + '/messages.json')
+    
+    client.messages.create({ 
+	to: "2395657035", 
+	from: "+12393301952", 
+	body: req.body.message,   
+}, function(err, message) { 
+	console.log(message.sid); 
+});
+
+    
+    res.send();
+});
+ 
+app.listen(port, function () {
+    console.log('Im watching you server 3000 @.@');
+});
+
+
